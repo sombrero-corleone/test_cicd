3061ecc51677687bce65aee847507bc543cb6e9e
sombrero-corleone <sgvonjoli@gmail.com>
2026-06-28 20:48:06 +0200

    test pipeline

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello, DevOps!");
+console.log("Learning CI/CD using Github Actions.");
+console.log("Tetsing pipeline immediately");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Starting tests...");
+setTimeout(() => console.log("Waiting three seconds"), 3000);
+console.log("Tests complete");
